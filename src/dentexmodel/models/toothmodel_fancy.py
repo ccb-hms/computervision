8980a02011064ef5b4042c171758a3725d7c9963
awerdich <awerdich@gmail.com>
2024-01-12 12:46:15 -0500

    New model with validation and logging.

--- src/dentexmodel/models/toothmodel_fancy.py
+++ src/dentexmodel/models/toothmodel_fancy.py
@@ -9,19 +9,44 @@ Harvard Medical School
 
 import logging
 from typing import Any
-
 # Pytorch imports
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision.models import resnet50, ResNet50_Weights
-
 # Lightning module
 from lightning.pytorch import LightningModule
 from lightning.pytorch.utilities.types import OptimizerLRScheduler, STEP_OUTPUT
 from lightning.pytorch.utilities.types import TRAIN_DATALOADERS, EVAL_DATALOADERS
+# Torchmetrics
+import torchmetrics.classification as tmc
 
 logger = logging.getLogger(name=__name__)
+
+
+def performance_metrics(metric_dict, logits, target, metric_prefix='train'):
+    """
+    Calculate performance metrics for a given set of logits and target labels.
+    """
+    preds = nn.Softmax(dim=1)(logits)
+    performance_dict = {}
+    for metric_name, metric in metric_dict.items():
+        performance_dict.update({f'{metric_prefix}_{metric_name}': metric(preds=preds, target=target)})
+    return performance_dict
+
+
+def average_performance_metrics(step_metrics_list, decimals=3):
+    """
+    Calculate the average performance from a list of dictionaries
+    """
+    average_metrics = {}
+    for metric in step_metrics_list[0].keys():
+        metric_value = torch.stack([x.get(metric) for x in step_metrics_list])
+        # Remove any zero values before averaging
+        metric_value = metric_value[metric_value.nonzero().squeeze()]
+        metric_value = metric_value.mean().detach().cpu().numpy().round(decimals)
+        average_metrics[metric] = metric_value
+    return average_metrics
 
 
 class ResNet50Model:
@@ -48,6 +73,7 @@ class ToothModel(LightningModule):
                  val_dataset,
                  test_dataset,
                  batch_size,
+                 num_classes,
                  num_workers=1,
                  lr=1.0e-3,
                  model=None):
@@ -57,6 +83,7 @@ class ToothModel(LightningModule):
         self.val_dataset = val_dataset
         self.test_dataset = test_dataset
         self.batch_size = batch_size
+        self.num_classes = num_classes
         self.num_workers = num_workers
         self.lr = lr
         self.decimals = 5
@@ -67,6 +94,17 @@ class ToothModel(LightningModule):
             self.model = model
         # Loss function
         self.criterion = nn.CrossEntropyLoss()
+        # Performance metrics
+        self.metrics = nn.ModuleDict({
+            'accuracy': tmc.MulticlassAccuracy(num_classes=num_classes, average='micro'),
+            'precision': tmc.MulticlassPrecision(num_classes=num_classes, average='macro'),
+            'recall': tmc.MulticlassRecall(num_classes=num_classes, average='macro'),
+            'f1': tmc.MulticlassF1Score(num_classes=num_classes, average='macro'),
+            'auroc': tmc.MulticlassAUROC(num_classes=num_classes, average='macro')
+        })
+        self.train_step_metrics_list = []
+        self.val_step_metrics_list = []
+        self.test_step_metrics_list = []
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
         dl = DataLoader(self.train_dataset,
@@ -77,7 +115,7 @@ class ToothModel(LightningModule):
         return dl
 
     def val_dataloader(self) -> EVAL_DATALOADERS:
-        dl = DataLoader(self.val_datase,
+        dl = DataLoader(self.val_dataset,
                         batch_size=self.batch_size,
                         num_workers=self.num_workers,
                         shuffle=False,
@@ -97,9 +135,29 @@ class ToothModel(LightningModule):
         return x
 
     def training_step(self, batch, batch_idx, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
-        image, label = batch
+        image, target = batch
         pred = self.forward(image)
-        loss = self.criterion(pred, label)
+        loss = self.criterion(pred, target)
+        train_step_metrics = {'train_loss': loss}
+        performance_dict = performance_metrics(metric_dict=self.metrics,
+                                               logits=pred,
+                                               target=target,
+                                               metric_prefix='train')
+        train_step_metrics.update(performance_dict)
+        self.train_step_metrics_list.append(train_step_metrics)
+        return loss
+
+    def validation_step(self, batch, batch_idx, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        image, target = batch
+        pred = self.forward(image)
+        loss = self.criterion(pred, target)
+        val_step_metrics = {'val_loss': loss}
+        performance_dict = performance_metrics(metric_dict=self.metrics,
+                                               logits=pred,
+                                               target=target,
+                                               metric_prefix='val')
+        val_step_metrics.update(performance_dict)
+        self.val_step_metrics_list.append(val_step_metrics)
         return loss
 
     def predict_step(self, batch, batch_idx, *args: Any, **kwargs: Any) -> Any:
@@ -110,3 +168,16 @@ class ToothModel(LightningModule):
     def configure_optimizers(self) -> OptimizerLRScheduler:
         opt = torch.optim.AdamW(self.parameters(), lr=self.lr)
         return opt
+
+    def on_train_epoch_end(self) -> None:
+        epoch_train_metrics = average_performance_metrics(step_metrics_list=self.train_step_metrics_list,
+                                                          decimals=self.decimals)
+        self.log_dict(epoch_train_metrics, prog_bar=True)
+        self.train_step_metrics_list.clear()
+
+    def on_validation_epoch_end(self) -> None:
+        epoch_val_metrics = average_performance_metrics(step_metrics_list=self.val_step_metrics_list,
+                                                        decimals=self.decimals)
+        self.log_dict(epoch_val_metrics, prog_bar=True)
+        self.val_step_metrics_list.clear()
+

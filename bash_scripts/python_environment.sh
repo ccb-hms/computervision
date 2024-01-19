aa172ed60a0a7f0d991111fe589378645bb61ee4
awerdich <awerdich@gmail.com>
2024-01-19 16:52:28 -0500

    useful srun commands.

--- bash_scripts/python_environment.sh
+++ bash_scripts/python_environment.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 module load gcc/9.2.0
-module load cuda/11.7
 module load python/3.10.11
 
-repo_dir="/home/aaw10/gitrepos/bigsmile"
+# CUDA UPDATES
+# Symbolic link sudo ln -s /home/andreas/cuda1120 /usr/local/cuda 
+export PATH="/home/aaw10/cuda/bin:${PATH:+:${PATH}}"
+export CUDA_PATH="/home/aaw10/cuda"
+export LD_LIBRARY_PATH="/home/aaw10/cuda/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/aaw10/cuda/extras/CUPTI/lib64"
+
+repo_dir="/home/aaw10/gitrepos/dentexmodel"
 cd ${repo_dir}
 pipenv shell

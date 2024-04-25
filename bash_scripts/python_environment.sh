5f7ad28f7c4c5cd970d9a8bb6c8d758106e98ad7
awerdich <awerdich@gmail.com>
2024-04-24 20:53:26 -0400

    Activate pipenv environment

--- bash_scripts/python_environment.sh
+++ bash_scripts/python_environment.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 module load gcc/9.2.0
 module load python/3.10.11
+module load cuda/12.1
 
 # CUDA UPDATES
 # Symbolic link sudo ln -s /home/andreas/cuda1120 /usr/local/cuda 
-export PATH="/home/aaw10/cuda/bin:${PATH:+:${PATH}}"
-export CUDA_PATH="/home/aaw10/cuda"
-export LD_LIBRARY_PATH="/home/aaw10/cuda/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/aaw10/cuda/extras/CUPTI/lib64"
+# export PATH="/home/aaw10/cuda/bin:${PATH:+:${PATH}}"
+# export CUDA_PATH="/home/aaw10/cuda"
+# export LD_LIBRARY_PATH="/home/aaw10/cuda/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/aaw10/cuda/extras/CUPTI/lib64"
 
-repo_dir="/home/aaw10/gitrepos/dentexmodel"
+repo_dir="/home/aaw10/gitrepos/computervision"
 cd ${repo_dir}
 pipenv shell

b099d94f6cc9560ec592fcaaed413fa955974491
Andreas Werdich <andreas_werdich@hms.harvard.edu>
2026-07-09 08:56:32 -0400

    Python pacakge file

--- src/computervision/__init__.py
+++ src/computervision/__init__.py
@@ -1,8 +1,13 @@
+from importlib.metadata import version, PackageNotFoundError
 from pathlib import Path
 
-# you can use os.path and open() as well
-__version__ = Path(__file__).parent.joinpath("VERSION").read_text()
 __authors__ = Path(__file__).parent.joinpath("AUTHORS").read_text()
 
+try:
+    __version__ = version('computervision')
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = 'unknown'
+
 def main() -> None:
-    print("Hello from the CCB computervision project!")
+    print('Hello from the CCB computervision project!')
